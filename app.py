32b7421dfc60792d87b4bb4eb14381c97627c3ec
KreyDev <quackface2@gmail.com>
2024-11-26 11:14:14 -0500

    fix fstring

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def machine_list(hall):
     wstr = f'<br><div>&emsp;Washers:&emsp;({wsum["available"]}/{wsum["available"] + wsum["inUse"] + wsum["temporarilyUnavailable"]})</div>'
     dstr = f'<br><div>&emsp;Dryers:&emsp;({dsum["available"]}/{dsum["available"] + dsum["inUse"] + dsum["temporarilyUnavailable"]})</div>'
     for m in sorted(mech, key=lambda x: x["stickerNumber"]):
-        mstring = f'<div>&emsp;&emsp;{m["stickerNumber"]}:&emsp;{((m["mode"] != "running" or m["available"] == True) and m["mode"] != "unknown") * "Available"}{(m["timeRemaining"] > 0) * f"In Use, {m["timeRemaining"]} min left"}{(m["mode"] == "unknown") * "[Unknown State]"}{(m["notAvailableReason"] == "offline") * " [Offline]"}</div>'
+        mstring = f'''<div>&emsp;&emsp;{m["stickerNumber"]}:&emsp;{((m["mode"] != "running" or m["available"] == True) and m["mode"] != "unknown") * "Available"}{(m["timeRemaining"] > 0) * f"In Use, {m["timeRemaining"]} min left"}{(m["mode"] == "unknown") * "[Unknown State]"}{(m["notAvailableReason"] == "offline") * " [Offline]"}</div>'''
         if (m["type"] == "washer"):
             wstr += mstring
         if (m["type"] == "dryer"):
