b005eac6abc9cddb457ed5ccb5fbf871e8d33244
KreyDev <quackface2@gmail.com>
2024-11-26 02:51:46 -0500

    saving and loading data

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import *
 from requests import get
 from json import loads
 from markupsafe import escape
+import pickle, time
+from datetime import datetime
 
 app = Flask(__name__)
 api = "https://mycscgo.com/api/v3/machine/info/"
@@ -11,6 +13,29 @@ halls:dict = {\
 "barton": "951c9953-0c62-4c0a-b57b-e93da388bad6",
 "hall": "94808e51-2044-4441-9b99-391c52dcdb32"
 }
+
+class Mach(object):
+    def __init__(self, _id:int, _type:str, doorClosed:bool, mode:str, available:bool, notAvailableReason:str, timeLeft:int) -> None:
+        self._id = _id
+        self._ty = _type
+        self.doorClosed = doorClosed
+        self.mode = mode
+        self.available = available
+        self.notAvailableReason = notAvailableReason
+        self.timeLeft = timeLeft
+    
+    def __str__(self) -> str:
+        return f"{self._ty[0]}{self._id}: {self.timeLeft}min {self.doorClosed * "door-closed"} {self.available * "available"} {self.notAvailableReason}"
+
+class Hall(object):
+    def __init__(self, name:str, machs:list) -> None:
+        self.name:str = name
+        self.machs:list = machs
+    
+    def __str__(self) -> str:
+        return f"{self.name} hall: {str([str(x) for x in self.machs])}"
+
+data = dict() # time keys, [hall list] vals
 
 styling = """
 <head>
@@ -40,6 +65,13 @@ def all_halls():
     for h in halls.keys():
         outstr += machine_list(h)
     return outstr
+
+@app.route("/save")
+def save_all_halls():
+    data[time.time()] = [Hall(h, [Mach(m["stickerNumber"], m["type"], m["doorClosed"], m["mode"], m["available"], m["notAvailableReason"], m["timeRemaining"]) for m in machines(h)["machines"]]) for h in halls.keys()]
+    # print(data)
+    save_data()
+    return styling + "data saved!"
 
 @app.route("/mach/<hall>")
 def machine_list(hall):
@@ -76,5 +108,33 @@ def get_mach(mach, hall):
 def get_halls():
     return styling + "Registered halls: " + ", ".join(halls.keys())
 
+def load_data():
+    global data
+    print("loading data...")
+    try:
+        with open("data.pkl", mode="rb") as f:
+            data = pickle.loads(f.read())
+            f.close()
+        # print(data)
+    except Exception as e: print(e)
+
+def save_data():
+    global data
+    print("saving data...")
+    with open("data.pkl", mode="wb") as f:
+        f.write(pickle.dumps(data))
+        f.close()
+
+@app.route("/view")
+def view():
+    load_data()
+    outstr = styling + ""
+    for t in data.keys():
+        outstr += datetime.fromtimestamp(t).strftime("<br>%A, %B %d, %Y %I:%M:%S ")
+        outstr += str([str(x) for x in data[t]])
+        outstr += "<br>"
+    return outstr
+
 if __name__ == "__main__": # guard
+    load_data()
     system("flask run")
