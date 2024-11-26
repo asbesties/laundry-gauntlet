2d3e30589d0ca213eabd2571e5f83aad2ab7d1fc
KreyDev <quackface2@gmail.com>
2024-11-26 01:02:01 -0500

    slightly unbreak hall

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ from json import loads
 from markupsafe import escape
 
 app = Flask(__name__)
+api = "https://mycscgo.com/api/v3/machine/info/"
 
-places:dict = {\
-"barton": "https://mycscgo.com/api/v3/machine/info/951c9953-0c62-4c0a-b57b-e93da388bad6",
+halls:dict = {\
+"barton": "951c9953-0c62-4c0a-b57b-e93da388bad6",
+"hall": "94808e51-2044-4441-9b99-391c52dcdb32"
 }
 
 styling = """
 <head>
+    <meta http-equiv="refresh" content="5">
     <title>Laundry Gauntlet</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -28,8 +31,15 @@ styling = """
 """
 
 def machines(hall):
-    machines = loads(get(places[hall]).text)
+    machines = loads(get(api + halls[hall]).text)
     return machines
+
+@app.route("/mach")
+def all_halls():
+    outstr = styling + ""
+    for h in halls.keys():
+        outstr += machine_list(h)
+    return outstr
 
 @app.route("/mach/<hall>")
 def machine_list(hall):
@@ -37,18 +47,18 @@ def machine_list(hall):
     dsum = machines(escape(hall))["availabilitySummary"]["dryers"]
     wsum = machines(escape(hall))["availabilitySummary"]["washers"]
     mech = machines(escape(hall))["machines"]
-    outstr = styling + f"<div>Machines Available in {hall.title()}:<br>&emsp;Washers:&emsp;{wsum["available"]}/{wsum["available"] + wsum["inUse"] + wsum["temporarilyUnavailable"]}<br>&emsp;Dryers:&emsp;{dsum["available"]}/{dsum["available"] + dsum["inUse"] + dsum["temporarilyUnavailable"]}</div>"
-    wstr = "<div>&emsp;Washers:<br>"
-    dstr = "<div>&emsp;Dryers:<br>"
+    outstr = styling + f"<div>Machines Available in {hall.title()}:<br>"
+    wstr = f"<br><div>&emsp;Washers:&emsp;({wsum["available"]}/{wsum["available"] + wsum["inUse"] + wsum["temporarilyUnavailable"]})</div>"
+    dstr = f"<br><div>&emsp;Dryers:&emsp;({dsum["available"]}/{dsum["available"] + dsum["inUse"] + dsum["temporarilyUnavailable"]})</div>"
     for m in sorted(mech, key=lambda x: x["stickerNumber"]):
-        mstring = f"<div>&emsp;&emsp;{m["stickerNumber"]}:&emsp;{(m["mode"] == "pressStart" or m["available"] == True) * "Available"}{(m["timeRemaining"] > 0 and m["notAvailableReason"] == "in-use") * f"In Use, {m["timeRemaining"]} min left"}{(m["mode"] == "unknown") * "<i>???</i>"}</div>"
+        mstring = f"<div>&emsp;&emsp;{m["stickerNumber"]}:&emsp;{((m["mode"] != "running" or m["available"] == True) and m["mode"] != "unknown") * "Available"}{(m["timeRemaining"] > 0) * f"In Use, {m["timeRemaining"]} min left"}{(m["mode"] == "unknown") * "[Unknown State]"}{(m["notAvailableReason"] == "offline") * " [Offline]"}</div>"
         if (m["type"] == "washer"):
             wstr += mstring
         if (m["type"] == "dryer"):
             dstr += mstring
     wstr += "</div>"
     dstr += "</div>"
-    outstr += wstr + dstr
+    outstr += wstr + dstr + "<br>"
     return outstr
 
 @app.route("/mach/<hall>/<mach>")
@@ -64,7 +74,7 @@ def get_mach(mach, hall):
 
 @app.route("/halls")
 def get_halls():
-    return styling + "Registered halls: " + ", ".join(places.keys())
+    return styling + "Registered halls: " + ", ".join(halls.keys())
 
 if __name__ == "__main__": # guard
     system("flask run")
