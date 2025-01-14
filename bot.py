635d161aebf5dcb7170674fbb5e300fb4ff9225d
KreyDev <quackface2@gmail.com>
2025-01-14 17:47:48 -0500

    change f strings and change dotenv

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import app
 import discord
-import dotenv
+import os
 
-token = dotenv.get_key("token")
+token = os.getenv("token")
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
@@ -13,18 +13,18 @@ async def cmd_halls(message):
 
 async def cmd_hall(message):
     hall = message.content.split(" ")[1].lower()
-    return (f"Machines in {hall.title()}: {", ".join([str(m["stickerNumber"]) for m in app.machines(hall)["machines"]])}")
+    return (f"Machines in {hall.title()}: {', '.join([str(m['stickerNumber']) for m in app.machines(hall)['machines']])}")
 
 async def cmd_time(message):
     hall = message.content.split(" ")[1].lower()
-    return ("\n".join([f"{app.get_mach(int(m), hall)["timeRemaining"]} mins on {hall.title()} {m}" for m in message.content.split(" ")[2:]]))
+    return ("\n".join([f"{app.get_mach(int(m), hall)['timeRemaining']} mins on {hall.title()} {m}" for m in message.content.split(' ')[2:]]))
 
 async def cmd_api(message):
     hall = message.content.split(" ")[1].lower()
-    return ("\n".join([f"Raw data from {hall.title()} {m}:```json\n{app.get_mach(int(m), hall)}\n```" for m in message.content.split(" ")[2:]]))
+    return ("\n".join([f"Raw data from {hall.title()} {m}:```json\n{app.get_mach(int(m), hall)}\n```" for m in message.content.split(' ')[2:]]))
 
 async def cmd_laundry(message):
-    return f"Laundry commands:\n{"\n".join(["/" + c for c in cmds.keys()])}\nUsage: `/command hall id`\nYou can chain ids like this: `/api barton 220 221 222`"
+    return "Laundry commands: + '\n'.join(['/' + c for c in cmds.keys()])" + "\nUsage: `/command hall id`\nYou can chain ids like this: `/api barton 220 221 222`"
 
 cmds = {
     "halls": cmd_halls,
@@ -45,7 +45,7 @@ async def on_message(message: discord.Message):
         if cmd in cmds:
             print(f"[CMD] @{message.author} used {message.content}")
             cmdout = await cmds[cmd](message)
-            print(f"[API] >> {cmdout.replace("\n", "\n[API] >> ")}")
+            print("[API] >> " + cmdout.replace('\n', '\n[API] >> '))
             await message.channel.send(cmdout)
 
 client.run(token)
