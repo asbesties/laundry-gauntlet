68e820fe88dbeeb66f8fc1ce3d30e59af399bb38
KreyDev <quackface2@gmail.com>
2025-01-15 00:41:13 -0500

    pull app reference

--- bot.py
+++ bot.py
@@ -10,22 +10,22 @@ intents.members = True
 intents.message_content = True
 client: discord.Client = discord.Client(intents=intents)
 
-async def cmd_halls(message):
+async def cmd_halls(message, app=app):
     return ("Registered halls: " + ", ".join(app.halls.keys()))
 
-async def cmd_hall(message):
+async def cmd_hall(message, app=app):
     hall = message.content.split(" ")[1].lower()
     return (f"Machines in {hall.title()}: {', '.join([str(m['stickerNumber']) for m in app.machines(hall)['machines']])}")
 
-async def cmd_time(message):
+async def cmd_time(message, app=app):
     hall = message.content.split(" ")[1].lower()
     return ("\n".join([f"{app.get_mach(int(m), hall)['timeRemaining']} mins on {hall.title()} {m}" for m in message.content.split(' ')[2:]]))
 
-async def cmd_api(message):
+async def cmd_api(message, app=app):
     hall = message.content.split(" ")[1].lower()
     return ("\n".join([f"Raw data from {hall.title()} {m}:```json\n{app.get_mach(int(m), hall)}\n```" for m in message.content.split(' ')[2:]]))
 
-async def cmd_laundry(message):
+async def cmd_laundry(message, app=app):
     return "Laundry commands: + '\n'.join(['/' + c for c in cmds.keys()])" + "\nUsage: `/command hall id`\nYou can chain ids like this: `/api barton 220 221 222`"
 
 cmds = {
