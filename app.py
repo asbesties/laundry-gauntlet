9237a5575f99385fe408a14a1cbf93de1130c826
AliasKugelblitz <prashantsadoo@gmail.com>
2025-02-07 18:56:59 -0500

    Introducing: The laundry gauntlet expansion pack! Featuring 5 new dorms!: BARH, Bray, Cary, Nugent, and Warren!

--- app.py
+++ app.py
@@ -10,10 +10,15 @@ app = Flask(__name__)
 api = "https://mycscgo.com/api/v3/machine/info/"
 
 halls:dict = {\
+"barh": "58b25d98-0c48-417e-88e2-dc14a11a27cf",
 "barton": "951c9953-0c62-4c0a-b57b-e93da388bad6",
+"bray": "86c2d26f-4337-4133-84f2-ec22f35458f4",
+"cary": "751e7889-268f-4e85-9c89-0c6c5ec23624",
 "crockett": "2531553f-e5ac-4b97-aa85-39d437cc6a1a",
 "hall": "94808e51-2044-4441-9b99-391c52dcdb32",
-"nason": "5aaa2c7a-7928-4103-9cec-755687952805"
+"nason": "5aaa2c7a-7928-4103-9cec-755687952805",
+"nugent": "62c3b774-6745-4d5a-a997-5132b510bb2a",
+"warren": "2cc3c4ef-bb23-429a-a8a8-3c5388c12b04"
 }
 
 class Mach(object):
